b755d06cdeea6ae642219be7c08fe4b24c9ed409
Nikita Fomichev <nikita.fomichev@glovoapp.com>
2023-04-28 19:48:32 +0200

    avoid overlapping keybinds between lazygit and terminal toggle

--- config.lua
+++ config.lua
@@ -89,7 +89,6 @@ lvim.builtin.treesitter.rainbow.enable = true
 
 function _G.set_terminal_keymaps()
         local opts = { buffer = 0 }
-        vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
         vim.keymap.set('t', '<leader>h', [[<Cmd>wincmd h<CR>]], opts)
         vim.keymap.set('t', '<leader>j', [[<Cmd>wincmd j<CR>]], opts)
